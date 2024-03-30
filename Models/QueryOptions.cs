7543740df9e25127d2b6a67a9a9dec7d862ac4f0
Carson-A01 <156151607+Carson-A01@users.noreply.github.com>
2024-03-30 17:27:54 -0500

    added generic Repository and QueryOptions classes

---
+++ Models/QueryOptions.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+
+namespace ToDoListApp.Models
+{
+    public class QueryOptions<T>
+    {
+        public Expression<Func<T, Object>> OrderBy { get; set; } = null!;
+        public Expression<Func<T, bool>> Where { get; set; } = null!;
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        private string[] includes = Array.Empty<String>();
+        public string Includes { set => includes = value.Replace(" ", "").Split(','); }
+        public string[] GetIncludes() => includes;
+        public bool HasOrderBy => OrderBy != null;
+        public bool HasWhere => Where != null;
+        public bool HasPaging => PageNumber > 0 && PageSize > 0;
+    }
+
+    public static class QueryExtensions
+    {
+        public static IQueryable<T> PageBy<T>(this IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            return query.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoListApp.Models
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> List(QueryOptions<T> options);
+        T? Get(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected ToDoContext context { get; set; }
+        private DbSet<T> dbset { get; set; }
+        public Repository(ToDoContext ctx)
+        {
+            context = ctx;
+            dbset = context.Set<T>();
+        }
+        public virtual IEnumerable<T> List(QueryOptions<T> options)
+        {
+            IQueryable<T> query = dbset;
+            foreach (string include in options.GetIncludes())
+            {
+                query = query.Include(include);
+            }
+            if (options.HasWhere) { query = query.Where(options.Where); }
+            if (options.HasOrderBy) { query = query.OrderBy(options.OrderBy); }
+            if (options.HasPaging) { query = query.PageBy(options.PageNumber, options.PageSize); }
+            return query.ToList();
+        }
+
+        public virtual T? Get(int id) => dbset.Find(id);
+        public virtual void Insert(T entity) => dbset.Add(entity);
+        public virtual void Update(T entity) => dbset.Update(entity);
+        public virtual void Delete(T entity) => dbset.Remove(entity);
+        public virtual void Save() => context.SaveChanges();
+    }
+}
