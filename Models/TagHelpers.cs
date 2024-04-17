5ecfd92f7b525b0efe533b5b5c3d43b9e350824e
Carson-A01 <156151607+Carson-A01@users.noreply.github.com>
2024-04-17 13:33:56 -0500

    Added TagHelpers and Partial View

---
+++ Models/TagHelpers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Nest;
+
+namespace ToDoListApp.Models
+{
+    public class TagHelpers
+    {
+        public class SubmitButtonTagHelper : TagHelper
+        {
+            public override void Process(TagHelperContext context,
+            TagHelperOutput output)
+            {
+                // make it a button element with start and end tags
+                output.TagName = "button";
+                output.TagMode = TagMode.StartTagAndEndTag;
+                // make it a submit button
+                output.Attributes.SetAttribute("type", "submit");
+                // append bootstrap button classes
+                string newClasses = "btn btn-primary";
+                string oldClasses =
+                output.Attributes["class"]?.Value.ToString() ?? "";
+                string classes = (string.IsNullOrEmpty(oldClasses)) ?
+                newClasses : $"{oldClasses} {newClasses}";
+                output.Attributes.SetAttribute("class", classes);
+            }
+        }
+    }
+}
